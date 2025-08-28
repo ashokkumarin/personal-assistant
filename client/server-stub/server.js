aa7808029d16b10c61fd65ca97ea983d699311ef
Ashok <aashokkumarin@hotmail.com>
2025-08-28 21:44:29 +0530

    add CORS in server stub

--- client/server-stub/server.js
+++ client/server-stub/server.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const app = express()
+
+// Add CORS middleware to allow requests from Vite dev server
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200)
+  } else {
+    next()
+  }
+})
+
 app.use(express.json())
 
 
